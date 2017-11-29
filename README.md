064fec3f967dcccb577bbfade5d2bed736ed1593
adamo57 <adamouellette57@gmail.com>
2017-11-28 20:15:06 -0500

    Tweeting

--- README.md
+++ README.md
@@ -6,5 +6,9 @@ Go lambda job for reading tweets from a user
  
  `export TWITTER_CONSUMER_SECRET=<twitter_consumer_secret>`
  
+  `export TWITTER_ACCESS_TOKEN=<twitter_access_key>`
+  
+  `export TWITTER_ACCESS_TOKEN_SECRET=<twitter_access_token_secret>`
+ 
 ### Run Tests
   `go test ./...`
--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
+//import "github.com/eawsy/aws-lambda-go-core/service/lambda/runtime"
+
+import (
+	"github.com/ChimeraCoder/anaconda"
+	"os"
+	"fmt"
+	"net/url"
+	"log"
+)
+
 func init() {
 
 }
 
-func main() {
+//Handle is what we will want to call when we invoke the lambda job
+//func Handle(evt interface{}, ctx *runtime.Context) (string, error) {
+//	return "Hello, World!", nil
+//}
 
+func main() {
+	consumerKey := os.Getenv("TWITTER_CONSUMER_KEY")
+	consumerSecret := os.Getenv("TWITTER_CONSUMER_SECRET")
+	accessToken := os.Getenv("TWITTER_ACCESS_TOKEN")
+	accessTokenSecret := os.Getenv("TWITTER_ACCESS_TOKEN_SECRET")
+
+	anaconda.SetConsumerKey(consumerKey)
+	anaconda.SetConsumerSecret(consumerSecret)
+
+	api := anaconda.NewTwitterApi(accessToken, accessTokenSecret)
+
+	result, err := api.PostTweet("This is posted with <3 from golang", nil)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	log.Println(result.FullText)
 }
