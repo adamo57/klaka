69080daedb5298821136043c4b5665e31fd46d25
adamo57 <adamouellette57@gmail.com>
2017-11-30 21:02:11 -0500

    Able to post tweet with the correct creds for one account

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/adamo57/klaka/db"
+	"github.com/adamo57/klaka/twitter"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -44,5 +45,15 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		//post a tweet with the creds of the user(s) in question
+		api := twitter.NewTwitterClient(results.Twitter.OAuthToken, results.Twitter.OAuthTokenSecret)
+
+		tweet, err := api.PostTweet("Posted Tweet.", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println(tweet.FullText)
 	}
 }
